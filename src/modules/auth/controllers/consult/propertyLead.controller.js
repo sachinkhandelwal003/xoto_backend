7cc9c51229a2af87f39db36de5c254d393cbe880
Surya Pratap Singh <surya@kotiboxglobaltech.com>
2026-01-15 16:33:57 +0530

    code refactored

--- src/modules/auth/controllers/consult/propertyLead.controller.js
+++ src/modules/auth/controllers/consult/propertyLead.controller.js
@@ -3,7 +3,7 @@ const PropertyLead = require('../../models/consultant/propertyLead.model');
 const { StatusCodes } = require('../../../../utils/constants/statusCodes');
 const { APIError } = require('../../../../utils/errorHandler');
 const asyncHandler = require('../../../../utils/asyncHandler');
-
+const MortgageApplication = require("../../../mortgages/models/index.js");
 // Create
 exports.createPropertyLead = asyncHandler(async (req, res) => {
   let data = req.body;
@@ -99,7 +99,34 @@ exports.updatePropertyLead = asyncHandler(async (req, res) => {
 // Update
 exports.createMortgagePropertyLead = asyncHandler(async (req, res) => {
   const lead = await PropertyLead.create({ ...req.body });
-  res.json({ success: true, message: 'Created', data: lead });
+
+  let mortgageApplication = {};
+
+  if (lead.type === "mortgage") {
+
+    // map lead_sub_type → loan_type
+    let loanType = "purchase";
+    if (lead.lead_sub_type === "refinance") loanType = "refinance";
+    if (lead.lead_sub_type === "buy_out") loanType = "buy_out";
+
+    mortgageApplication = await MortgageApplication.create({
+      application_id: `XOTO-${Math.random().toString(36).substring(2, 8).toUpperCase()}`,
+      lead_id: lead._id,
+
+      loan_type: loanType,
+      mortgage_type: "-",       // user hasn’t selected yet
+      loan_preference: "-",     // user hasn’t selected yet
+
+      income_type: lead.occupation || null,
+      property_value: lead.price || null,
+      loan_amount: null,
+
+      status: "in_progress"
+    });
+  }
+
+
+  res.json({ success: true, message: 'Created', data: {lead,mortgageApplication} });
 });
 
 // Mark Contacted
