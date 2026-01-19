b9661877c53e07e8f280affe76a6ce71b28c48e5
Surya Pratap Singh <surya@kotiboxglobaltech.com>
2026-01-19 17:53:33 +0530

    code refactored

--- src/modules/agency/controllers/index.js
+++ src/modules/agency/controllers/index.js
@@ -27,6 +27,7 @@ const agencySignup = async (req, res) => {
         await Otp.create({
             email,
             otp,
+            purpose: "agency_signup",
             expiresAt: Date.now() + 5 * 60 * 1000
         });
 
@@ -45,4 +46,57 @@ const agencySignup = async (req, res) => {
     }
 }
 
-export { agencySignup }
+const verifyOTP = async (req, res) => {
+    try {
+        let { otp, email } = req.body;
+        const threeMinutesAgo = new Date(Date.now() - 3 * 60 * 1000);
+        let newotp = Number(otp);
+        console.log("nbodyyyyyyyyyyyyyyyyyyyyyyyyyyyyy",req.body)
+        let verifyOTP = await Otp.findOne({ otp: newotp, email, purpose: "agency_signup", createdAt: { $gte: threeMinutesAgo } });
+        console.log("nbodyyyyyyyyyyyyyyyyyyyyyyyyyyyyy",verifyOTP)
+
+
+
+        if (!verifyOTP) {
+            return res.status(400).json({
+                success: false,
+                message: "No otp found"
+            })
+        }
+
+
+        const existingAgency = await Agency.findOne({ email });
+
+        if (existingAgency) {
+            return res.status(400).json({
+                success: false,
+                message: "Agency already registered"
+            });
+        }
+
+        const newagency = await Agency.create({ subscription_status: "free", otp_verified: true, is_active: false, ...req.body })
+
+        // const otp = Math.floor(100000 + Math.random() * 900000);
+
+        await Otp.deleteMany({
+            email,
+            purpose: "agency_signup",
+        });
+
+
+        return res.json({
+            success: true,
+            message: "OTP VERIFIED SUCCESSFULLY",
+            newagency
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: false,
+            message: `Error came ${error}`
+        })
+    }
+}
+
+
+
+export { agencySignup, verifyOTP }
--- src/modules/agency/routes/index.js
+++ src/modules/agency/routes/index.js
@@ -1,10 +1,11 @@
 
 const express = require("express");
-const {agencySignup} = require("../controllers/index.js");
+const {agencySignup,verifyOTP} = require("../controllers/index.js");
 
 const router = express.Router();
 
 //agency signup
 router.post("/agency-signup",agencySignup)
+router.post("/agency-signup/verify-otp",verifyOTP)
 
 module.exports = router;
