6c38284d4bc433b52a2f977ded079707f0950778
Surya Pratap Singh <surya@kotiboxglobaltech.com>
2026-02-03 19:00:15 +0530

    code refacyord

--- src/modules/auth/controllers/freelancer/models/MileStoneBill.js
+++ src/modules/auth/controllers/freelancer/models/MileStoneBill.js
@@ -2,35 +2,40 @@
 const mongoose = require('mongoose');
 
 const mileStonebillSchema = new mongoose.Schema({
-    project_id:{
-        type:mongoose.Types.ObjectId,
-        ref:"Project_freelancer",
-        required:false
+    project_id: {
+        type: mongoose.Types.ObjectId,
+        ref: "Project_freelancer",
+        required: false
+    },
+    paid_by_customer: {
+        type: Boolean,
+        default: false,
+        required: false
     },
     customer_id: {
         type: mongoose.Types.ObjectId,
         ref: "Customer",
-        required:false
+        required: false
     },
-    milestone_id:{
-        type:String,
-        default:"",
-        required:false
+    milestone_id: {
+        type: String,
+        default: "",
+        required: false
     },
     price: {
         type: Number,
         default: 0,
         required: false
-    }, 
+    },
     estimate_id: {
         type: mongoose.Types.ObjectId,
         ref: "Estimate",
-        required:false
+        required: false
     },
-    is_paid:{
-        type:Boolean,
-        required:false,
-        default:false,
+    is_paid: {
+        type: Boolean,
+        required: false,
+        default: false,
     }
 }, { timestamps: true });
 
