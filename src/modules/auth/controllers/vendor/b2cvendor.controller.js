f60f968de27d24b76a202aa0769e1abb7dacfc8a
Surya Pratap Singh <surya@kotiboxglobaltech.com>
2026-02-21 16:38:10 +0530

    new comit

--- src/modules/auth/controllers/vendor/b2cvendor.controller.js
+++ src/modules/auth/controllers/vendor/b2cvendor.controller.js
@@ -21,7 +21,7 @@ const logger = winston.createLogger({
   ]
 });
 
-// Vendor Login
+// Vendor Login fsfsfsfs
 exports.vendorLogin = asyncHandler(async (req, res, next) => {
   try {
     const { email, password } = req.body;
