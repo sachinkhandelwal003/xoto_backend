c1fe32dd4eac9a4c06c732b0addde12a1c66c740
Surya Pratap Singh <surya@kotiboxglobaltech.com>
2026-03-19 15:36:17 +0530

    dsfsd

--- src/middleware/s3Upload.js
+++ src/middleware/s3Upload.js
@@ -11,7 +11,10 @@ const upload = multer({
       const fileName = `properties/${Date.now()}-${file.originalname}`;
       cb(null, fileName);
     }
-  })
+  }),
+  limits: {
+    fileSize: 50 * 1024 * 1024 // 50MB
+  }
 });
 
 // AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY,AWS_REGION,AWS_S3_BUCKET
