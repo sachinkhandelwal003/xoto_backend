6c6511e5434837ff0fa07cc4c18007359ad4e21b
Surya Pratap Singh <surya@kotiboxglobaltech.com>
2026-04-09 14:54:11 +0530

    new comit

--- src/modules/vault/routes/lead.routes.js
+++ src/modules/vault/routes/lead.routes.js
@@ -7,7 +7,8 @@ const router = express.Router();
 // Agent routessds
 router.post('/create', protectVaultAgent, createLead);
 router.get('/my-leads', protectVaultAgent, getMyLeads);
-router.get('/:id', protect, getLeadById);
+router.get('/:id', protectVaultAgent, getLeadById);
+router.get('/admin/:id', protect, getLeadById);
 
 // Admin routes
 router.get('/admin/all', adminGetAllLeads);
