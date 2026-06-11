986741ffa0979a0df966a820e52d0071ad80fbd3
Shivam Mishra <shivammishrasm597364@gmail.com>
2026-06-11 13:28:28 +0530

    new code

--- src/modules/properties/routes/document.routes.js
+++ src/modules/properties/routes/document.routes.js
@@ -5,20 +5,20 @@ const {
   updateDocument,
   deleteDocument,
 } = require("../controllers/document.controller");
-const { protect } = require("../../../middleware/auth");
+const { protectMulti } = require("../../../middleware/auth");
 
 const router = express.Router();
 
 // Upload a document for a property
-router.post("/", protect, uploadDocument);
+router.post("/", protectMulti, uploadDocument);
 
 // Get all documents for a property (role-filtered)
-router.get("/:propertyId", protect, getDocumentsByProperty);
+router.get("/:propertyId", protectMulti, getDocumentsByProperty);
 
 // Update document metadata / visibility
-router.patch("/:docId", protect, updateDocument);
+router.patch("/:docId", protectMulti, updateDocument);
 
 // Soft-delete a document
-router.delete("/:docId", protect, deleteDocument);
+router.delete("/:docId", protectMulti, deleteDocument);
 
 module.exports = router;
